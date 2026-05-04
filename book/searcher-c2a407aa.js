58fa9136141650b4b48f5392eecfe59ab336bda2
mszczodrak <mszczodrak@users.noreply.github.com>
2026-05-04 00:33:09 +0000

    deploy: f3b1091743fa570fcef1517a2e7f358e115d8837

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b5759738.js',
+                path_to_root + 'searchindex-4f513c25.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
